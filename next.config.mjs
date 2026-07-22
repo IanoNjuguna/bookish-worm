bf3d29f7c550aedfa005fd0b162029e3afb27c6f
IanoNjuguna <inc3099@gmail.com>
2026-07-22 18:27:59 +0300

    swap '' for ``

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
     return [
       {
         source: '/api-backend/:path*',
-        destination: '${process.env.NEXT_PUBLIC_API_URL}/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ]
   },
